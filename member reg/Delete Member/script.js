42025941e20da53cb2c808474e1fa3444518ef3e
Iresh Nimantha <167967700+Iresh-Nimantha@users.noreply.github.com>
2024-06-09 12:15:26 +0530

    Delete member reg directory

--- member reg/Delete Member/script.js
+++
@@ -1,8 +0,0 @@
-function confirmDelete() {
-    var memberId = document.getElementById('memberId').value;
-    if (memberId === "") {
-        alert("Please enter a Member ID.");
-        return false;
-    }
-    return confirm("Delete relevant member?");
-}
