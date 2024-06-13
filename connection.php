c89ca6b7162949d12c7883912e6f77b6464b6dbc
👽 LAKRUWAN <lakruwaninguruwatte002@gmail.com>
2024-06-13 11:09:40 +0530

    Update connection.php
    fixed unnecessary code part

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost"; // replace with your database server name
-$username = "root"; // replace with your database username
-$password = ""; // replace with your database password
-$dbname = "library_system"; // replace with your database name
+$servername = "localhost";
+$username = "root"; 
+$password = "";
+$dbname = "library_system";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
