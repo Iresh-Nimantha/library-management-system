929980dddf712551d315664ac1ae4f03ebedf575
Kezor.zero <85814591+NKG-S@users.noreply.github.com>
2024-06-13 11:32:10 +0530

    process.php

--- library-management-system-master/book registration/process.php
+++ library-management-system-master/book registration/process.php
@@ -144,4 +144,5 @@ $conn->close();
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
