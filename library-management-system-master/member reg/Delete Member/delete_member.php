ab22785677c8389fb40a829d1c13fc6c017225b5
Kezor.zero <85814591+NKG-S@users.noreply.github.com>
2024-06-14 22:44:37 +0530

    Update delete_member.php

--- library-management-system-master/member reg/Delete Member/delete_member.php
+++ library-management-system-master/member reg/Delete Member/delete_member.php
@@ -1,30 +1,32 @@
 <?php
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "library_system";
-    
-    
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    
-    if ($conn->connect_error) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "library_system";
 
-    if($_SERVER['REQUEST_METHOD']=="POST"){
-        $member_id = $_POST['memberId'];
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-        $sql = "DELETE FROM member WHERE member_id = '$member_id'";
 
-        $result = $conn->query($sql);
+if ($conn->connect_error) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
-        if($result){
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $member_id = $_POST['memberId'];
+
+    $checkSql = "SELECT * FROM member WHERE member_id = '$member_id'";
+    $checkResult = $conn->query($checkSql);
+
+    if ($checkResult->num_rows > 0) {
+        $deleteSql = "DELETE FROM member WHERE member_id = '$member_id'";
+        if ($conn->query($deleteSql) === TRUE) {
             die("Member deleted successfully");
+        } else {
+            die("Error deleting member: " . $conn->error);
         }
-        else{
-            die("No member from this member id");
-        }
+    } else {
+        die("Invalid user or user already deleted");
     }
-    
+}
 ?>
